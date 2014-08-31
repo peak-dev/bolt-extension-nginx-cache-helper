f704b80eaacdbd5f945c7c32a800673a8cc4114c
Gawain Lynch <gawain.lynch@gmail.com>
2014-08-31 16:45:41 +0100

    First pass at functional code

--- Extension.php
+++ Extension.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Bolt\Extension\Bolt\NginxCache;
+namespace Bolt\Extension\Bolt\NginxCacheHelper;
+
+use Bolt\StorageEvents;
 
 class Extension extends \Bolt\BaseExtension
 {
@@ -10,7 +12,42 @@ class Extension extends \Bolt\BaseExtension
         return "NginXCacheHelper";
     }
 
-    public function initialize()
+    public function initialize() {
+        $this->app['dispatcher']->addListener(StorageEvents::POST_SAVE, array($this, 'hookPostSave'));
+    }
+
+    public function hookPostSave($input) {
+
+        // Get the content
+        $content = $input->getContent();
+        $host = $this->app['paths']['hosturl'];
+
+        // Purge it
+        $this->callPurgeFastCGIUrl($host . $this->config['purge']);
+        $this->callPurgeFastCGIUrl($host . $content->link() . $this->config['purge']);
+    }
+
+    /**
+     *
+     * @param string $url
+     */
+    private function callPurgeFastCGIUrl($url)
     {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_exec($ch);
+        curl_close($ch);
+    }
+
+    /**
+     * Default config
+     *
+     * @return array
+     */
+    protected function getDefaultConfig()
+    {
+        return array(
+            'nginx_purge_uri' => 'purge'
+        );
     }
 }
